b124cffaa627ee11c69fdda7c9dedfa90badf69d
Simon Boivin <sboivin@systra.com>
2023-11-06 16:59:51 -0500

    add some direction dependancies. not done

--- bike.py
+++ bike.py
@@ -93,8 +93,13 @@ def get_bicycle_oneway(links: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
 def extended_bicycle_process(links):
     links['tags'] = links['tags'].astype(str)
     links['tags'] = links['tags'].apply(lambda x: x.replace("'",'"'))
-    links['cycleway'] = links.apply(classify_cycleway_tags, result_type='expand', axis=1)
+    links['cycleway'] = links.apply(classify_cycleway_tags_with_direction, axis=1)
     links['cycleway_reverse'] = links['cycleway']
+    #links.loc[links['cycleway'] == "Dedicated oneway bike path",'cycleway_reverse'] = "No"
+    # we have tuple(left,right). cycleway is right lane (in country whit right side driving)
+    links['cycleway_reverse'] = links['cycleway_reverse'].apply(lambda x: x[0] if type(x)==tuple  else x )
+    links['cycleway'] = links['cycleway'].apply(lambda x: x[1] if type(x)==tuple  else x )
+
     return links
 
 # tags classification function
@@ -125,7 +130,7 @@ def classify_cycleway_tags(df):
         if osmTags.find('"bicycle": "yes"')>=0 or osmTags.find('"bicycle": "designated"')>=0:
             return "Pedestrian path/street with cycling allowed"
         else:
-            return "no" #"Pedestrian path/street with cycling not allowed"
+            return "No" #"Pedestrian path/street with cycling not allowed"
     elif highwayTag=="living_street":
         return "Shared zone"
     elif highwayTag=="construction":
@@ -234,3 +239,153 @@ def classify_cycleway_tags(df):
             return "No" #"Mixed traffic"
         #other
 
+# tags classification function
+def classify_cycleway_tags_with_direction(df):
+    osmTags=df['tags']
+    highwayTag=df['highway']
+    res=['No','No']
+    if highwayTag=='cycleway':
+        if  osmTags.find('"foot": "no"')>=0 and \
+        osmTags.find('"oneway": "yes"')>=0:
+            return ("Dedicated bike path","No")
+        elif osmTags.find('"foot": "no"')>=0:
+            return "Dedicated bike path"
+        else:
+            return "Shared bike path"
+    elif highwayTag=="path":
+        if (osmTags.find('"bicycle": "yes"')>=0 or \
+        osmTags.find('"bicycle": "designated"')>=0) and \
+        osmTags.find('"foot": "no"')>=0:
+            return "Dedicated bike path"
+        elif osmTags.find('"bicycle": "yes"')>=0 or \
+        osmTags.find('"bicycle": "designated"')>=0 and \
+        osmTags.find('"foot": "yes"')>=0:
+            return "Shared bike path"
+        else:
+            return "Pedestrian path/street with cycling not allowed"
+    elif highwayTag=="footway" or highwayTag=="pedestrian":
+        if osmTags.find('"bicycle": "yes"')>=0 or osmTags.find('"bicycle": "designated"')>=0:
+            return "Pedestrian path/street with cycling allowed"
+        else:
+            return "No" #"Pedestrian path/street with cycling not allowed"
+    elif highwayTag=="living_street":
+        return "Shared zone"
+    elif highwayTag=="construction":
+        if osmTags.find('"cycleway": "track"')>=0:
+            return "Future cycleway"
+        else:
+            return "Future link"
+    else:
+        if osmTags.find('"cycleway": "track"')>=0 or \
+        osmTags.find('"cycleway": "opposite_track"')>=0 or \
+        osmTags.find('"cycleway:both": "track"')>=0 :
+            return "Protected bike lane"
+        
+        else:
+            if osmTags.find('"cycleway:left": "track"')>=0:
+                res[0]="Protected bike lane"
+            if osmTags.find('"cycleway:right": "track"')>=0:
+                res[1]="Protected bike lane"
+            
+        if osmTags.find('"cycleway:both": "lane"')>=0 and \
+        (osmTags.find('"cycleway:both:lane": "exclusive"')>=0 or \
+        osmTags.find('"cycleway:left:lane": "exclusive"')>=0 ) and \
+        (osmTags.find('"cycleway:left:buffer:right"')>=0 or\
+        osmTags.find('"cycleway:both:buffer:right"')>=0 )and \
+        (osmTags.find('"cycleway:left:osm:left"')==-1 or \
+        osmTags.find('"cycleway:both:buffer:left"')==-1) or \
+        (osmTags.find('"cycleway:left": "lane"')>=0 and \
+        osmTags.find('"cycleway:left:lane": "exclusive"')>=0 and \
+        osmTags.find('"cycleway:left:buffer:right"')>=0 and \
+        osmTags.find('"cycleway:left:buffer:left"')==-1):
+            return "Buffered bike lane (road-side)" 
+            
+        elif osmTags.find('"cycleway:both": "lane"')>=0 and \
+        (osmTags.find('"cycleway:both:lane": "exclusive"')>=0 or \
+        osmTags.find('"cycleway:left:lane": "exclusive"')>=0 ) and \
+        (osmTags.find('"cycleway:left:buffer:left"')>=0 or\
+        osmTags.find('"cycleway:both:buffer:left"')>=0 )and \
+        (osmTags.find('"cycleway:left:buffer:right"')==-1 or \
+        osmTags.find('"cycleway:both:buffer:right"')==-1) or \
+        (osmTags.find('"cycleway:left": "lane"')>=0 and \
+        osmTags.find('"cycleway:left:lane": "exclusive"')>=0 and \
+        osmTags.find('"cycleway:left:buffer:left"')>=0 and \
+        osmTags.find('"cycleway:left:buffer:right"')==-1):
+            return "Buffered bike lane (kerb-side)" 
+            
+        elif osmTags.find('"cycleway:both": "lane"')>=0 and \
+        (osmTags.find('"cycleway:both:lane": "exclusive"')>=0 or \
+        osmTags.find('"cycleway:left:lane": "exclusive"')>=0 ) and \
+        ((osmTags.find('"cycleway:left:buffer:both"')>=0 or\
+        osmTags.find('"cycleway:both:buffer:both"')>=0 ) or \
+        (osmTags.find('"cycleway:left:buffer:left"')>=0 and \
+        osmTags.find('"cycleway:left:buffer:right"')>=0) or \
+        (osmTags.find('"cycleway:both:buffer:left"')>=0 and \
+        osmTags.find('"cycleway:both:buffer:right"')>=0)) or \
+        (osmTags.find('"cycleway:left": "lane"')>=0 and \
+        osmTags.find('"cycleway:left:lane": "exclusive"')>=0 and \
+        ((osmTags.find('"cycleway:left:buffer:left"')>=0 and \
+        osmTags.find('"cycleway:left:buffer:right"')>=0) or \
+        osmTags.find('"cycleway:left:buffer:both"')>=0)):
+            return "Buffered bike lane (both sides)" 
+            
+        elif osmTags.find('"cycleway": "lane"')>=0 or \
+        osmTags.find('"cycleway": "opposite_lane"')>=0 or \
+        osmTags.find('"cycleway:left": "lane"')>=0 or \
+        osmTags.find('"cycleway:right": "lane"')>=0 or \
+        osmTags.find('"cycleway:both": "lane"')>=0:
+            if (osmTags.find('"cycleway:both": "lane"')>=0 and \
+            (osmTags.find('"cycleway:both:lane": "advisory"')>=0 or \
+            osmTags.find('"cycleway:left:lane": "advisory"')>=0)) or\
+            osmTags.find('"cycleway:left": "lane"')>=0 and \
+            osmTags.find('"cycleway:left:lane": "advisory"')>=0:
+                if osmTags.find('"cycleway:left": "lane"')>=0 and \
+                osmTags.find('"cycleway:right": "no"')>=0 and \
+                osmTags.find('"cycleway:left:lane": "advisory"')>=0:
+                    return "Advisory bike lane (single side)"
+                else:
+                    return "Advisory bike lane"
+            elif osmTags.find('"cycleway:both:conditional"')>=0 or \
+            osmTags.find('"cycleway:left:conditional"')>=0 or \
+            osmTags.find('"cycleway:both:lane:conditional"')>=0 or \
+            osmTags.find('"cycleway:left:lane:conditional"')>=0:
+                return "Peak hour bike lane"
+            else :
+                if osmTags.find('"cycleway": "lane"')>=0 or \
+                osmTags.find('"cycleway": "opposite_lane"')>=0 or \
+                osmTags.find('"cycleway:both": "lane"')>=0:
+                    return "Painted bike lane" 
+                if osmTags.find('"cycleway:left": "lane"')>=0:
+                    res[0] =  "Painted bike lane" 
+                if osmTags.find('"cycleway:right": "lane"')>=0:
+                    res[1] =  "Painted bike lane" 
+        if osmTags.find('"cycleway": "shared_lane"')>=0 or \
+        (osmTags.find('"cycleway:both": "shared_lane"')>=0 and \
+        osmTags.find('"cycleway:both:lane": "pictogram"')>=0):
+            return "Sharrow"
+        else:
+            if osmTags.find('"cycleway:left:lane": "pictogram"')>=0 or \
+            osmTags.find('"cycleway:left": "shared_lane"')>=0:
+                res[0] = "Sharrow"
+            if osmTags.find('"cycleway:right:lane": "pictogram"')>=0 or \
+            osmTags.find('"cycleway:right": "shared_lane"')>=0:
+                res[1] = "Sharrow"
+        
+        if osmTags.find('"cycleway:both": "share_busway"')>=0 or \
+        osmTags.find('"cycleway:both": "opposite_share_busway"')>=0 or \
+        osmTags.find('"cycleway:left": "share_busway"')>=0 or \
+        osmTags.find('"cycleway:left": "opposite_share_busway"')>=0:
+            return "Bus lane with cycling allowed"
+        elif osmTags.find('"cycleway:left": "shoulder"')>=0 or \
+        osmTags.find('"cycleway:both": "shoulder"')>=0:
+            return "Shoulder cyclable"
+        elif osmTags.find('"bicycle": "dismount"')>=0:
+            return "Bicyclists dismount"
+        elif osmTags.find('"bicycle": "no"')>=0:
+            return "No" #"Cycling prohibited"
+        elif osmTags.find('"bike"')>=0 or \
+        osmTags.find('"cycle"')>=0:
+            return "Possible cycling infrastructure/link"
+        else:
+            return tuple(res)#"Mixed traffic"
+        #other
